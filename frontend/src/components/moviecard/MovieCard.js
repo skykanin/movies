9eaf8aa36c2ce0bf47ffba52d7eae341365cd53b
qwikk <44299295+qwakk@users.noreply.github.com>
2023-11-02 22:58:56 +0100

    fixed sizing of images inside cards

--- frontend/src/components/moviecard/MovieCard.js
+++ frontend/src/components/moviecard/MovieCard.js
@@ -5,12 +5,12 @@ const MovieCard = ({movie}) => {
         <div class="card mb-3" style={{maxWidth: "540px"}}>
             <div class="row g-0">
                 <div class="col-md-4">
-                    <img src={movie.posterLink} class="img-fluid rounded-start" alt="..." />
+                    <img src={movie.posterLink} class="img-fluid rounded-start" alt="..."/>
                 </div>
                 <div class="col-md-8">
                     <div class="card-body">
-                        <h5 class="card-title">{movie.seriesTitle}</h5>
-                        <p class="card-text">{movie.overview}</p>
+                        <h6 class="card-title">{movie.seriesTitle}</h6>
+                        <p class="card-text"><small>{movie.overview}</small></p>
                         <p class="card-text">{movie.imdbRating} / 10</p>
                     </div>
                 </div>

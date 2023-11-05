960933ddd485e3ab42c20d1ccc21901f11a5dc7a
qwikk <44299295+qwakk@users.noreply.github.com>
2023-11-05 23:02:24 +0100

    added a searchbar to the homepage

--- frontend/src/components/searchbar/SearchBar.js
+++ frontend/src/components/searchbar/SearchBar.js
@@ -1,18 +1,18 @@
-import React from 'react'
-import "./SearchBar.css"
+import './SearchBar.css'
 
 const SearchBar = () => {
-  return (
-    <div className='search-container'>
-        <div className='search-welcome'>
-            <h2>Welcome to qwikk.movies</h2>
-            <h4>Browse a collection of the top 1000 rated movies</h4>
-        </div>
-        <div className='search-bar'>
-            <h2>SearchBar</h2>
-        </div>
-    </div>
-  )
-}
-
-export default SearchBar
+    return (
+      <div class="container" style={{width: "900px"}}>
+            <div class="row height d-flex justify-content-center align-items-center">
+              <div class="col-md-6" style={{width: "100%"}}>
+                <div class="form">
+                  <i class="fa fa-search"></i>
+                  <input type="text" class="form-control form-input" placeholder="Search top rated movies!" />
+                </div>
+              </div>             
+            </div>           
+          </div>
+    )
+  }
+  
+  export default SearchBar

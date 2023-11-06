3c0acc24e3decca6f556b4254ac46d8a2646d2c1
qwikk <44299295+qwakk@users.noreply.github.com>
2023-11-06 17:30:52 +0100

    changed searchbar to material ui

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,18 +4,27 @@ import Home from './components/home/Home';
 import Header from './components/header/Header';
 import MoviePage from './components/moviepage/MoviePage';
 import Browse from './components/browse/Browse';
+import { createTheme, ThemeProvider } from '@mui/material';
 
 function App() {
 
+  const theme = createTheme({
+    palette: {
+      mode: "dark"
+    }
+  });
+
   return (
     <div className="App">
-      <Header/>
-      <Routes>
-        <Route path="" element={<Navigate replace to="home"/>}/>
-        <Route path="home" element={<Home />} />
-        <Route path="movie/:id" element={<MoviePage />} />
-        <Route path="browse" element={<Browse />} />
-      </Routes>
+      <ThemeProvider theme={theme}>
+        <Header/>
+        <Routes>
+          <Route path="" element={<Navigate replace to="home"/>}/>
+          <Route path="home" element={<Home />} />
+          <Route path="movie/:id" element={<MoviePage />} />
+          <Route path="browse" element={<Browse />} />
+        </Routes>
+      </ThemeProvider>
 
     </div>
   );
--- frontend/src/components/searchbar/SearchBar.js
+++ frontend/src/components/searchbar/SearchBar.js
@@ -1,17 +1,35 @@
+import { Button, FormGroup } from '@mui/material';
 import './SearchBar.css'
+import TextField from '@mui/material/TextField';
+
 
 const SearchBar = () => {
     return (
-      <div class="container" style={{width: "900px"}}>
-            <div class="row height d-flex justify-content-center align-items-center">
-              <div class="col-md-6" style={{width: "100%"}}>
-                <div class="form">
-                  <i class="fa fa-search"></i>
-                  <input type="text" class="form-control form-input" placeholder="Search top rated movies!" />
-                </div>
-              </div>             
-            </div>           
-          </div>
+      <div className='search-container'>
+        <FormGroup row>
+          <TextField
+            label="Search for top rated movies!"
+            id='fullWidth'
+            style={{width: "800px"}}
+            inputProps={{
+              style: {
+                color: "white",
+              }
+            }}
+            InputLabelProps={{
+              style: {
+                color: "white"
+              }
+            }}
+            fieldset>
+          </TextField>
+          <Button style={{width: "100px", 
+          background: "rgb(3, 37, 65)",
+          color: "white"}}
+          variant='contained' 
+          disableElevation>Search</Button>
+        </FormGroup>
+      </div>
     )
   }
   

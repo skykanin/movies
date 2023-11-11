2d7de7ccfd311738ee045cd5c41f6af617e9380d
qwikk <44299295+qwakk@users.noreply.github.com>
2023-11-11 18:10:40 +0100

    searchbar can now search with enter

--- frontend/src/components/searchbar/SearchBar.js
+++ frontend/src/components/searchbar/SearchBar.js
@@ -1,38 +1,37 @@
 import { Button, FormGroup } from '@mui/material';
 import './SearchBar.css'
 import TextField from '@mui/material/TextField';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { useState } from 'react';
 
 
 const SearchBar = () => {
 
     const [textField, setTextField] = useState("")
+    const navigate = useNavigate()
+
+    const onFormSubmit = (e) => {
+      e.preventDefault()
+      navigate(`/browse/title=${textField}`)
+    }
 
     return (
       <div className='search-container'>
-        <FormGroup row>
+        <form>
           <TextField
             onChange={text => {
               setTextField(text.target.value)}
             }
+            onSubmit={onFormSubmit}
             label="Search for top rated movies!"
             id='fullWidth'
             style={{width: "800px"}}
-            inputProps={{
-              style: {
-                color: "white",
-              }
-            }}
-            InputLabelProps={{
-              style: {
-                color: "white"
-              }
-            }}
-            fieldset>
+            fieldset
+            variant='filled'>
           </TextField>
             <Link to={`/browse/title=${textField}`} >
               <Button
+              type='submit'
               style={{width: "100px", height: "100%", 
               background: "rgb(3, 37, 65)",
               color: "white"}}
@@ -40,7 +39,7 @@ const SearchBar = () => {
               disableElevation>Search
               </Button>
             </Link>
-        </FormGroup>
+          </form>
       </div>
     )
   }

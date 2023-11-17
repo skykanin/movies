77009cda5ef8fcf96fc8ef8b2a2484bbeaf98d94
skykanin <3789764+skykanin@users.noreply.github.com>
2023-11-17 17:00:30 +0100

    Add movie icon to NavBar component

--- solid-frontend/src/components/navbar/NavBar.tsx
+++ solid-frontend/src/components/navbar/NavBar.tsx
@@ -1,13 +1,15 @@
 import { type Component } from 'solid-js'
 import style from './NavBar.module.css'
-import solidLogo from '../../assets/solid.svg'
+// import movieIcon from '../../assets/movie.svg'
 
 const NavBar: Component = () => {
   return (
     <header class={style.navbar}>
-      <div class={style.logo}>
+      <div>
         <a href="/">
-          <img src={solidLogo} class="logo solid" alt="Solid logo"/>
+          <svg class={style.logo} viewBox="0 0 24 24">
+            <path d="m18 4 2 4h-3l-2-4h-2l2 4h-3l-2-4H8l2 4H7L5 4H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V4h-4z"></path>
+          </svg>
         </a>
       </div>
       <h1>skykanin.movies</h1>
